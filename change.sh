a2c89094b30bb91e07407a8454d3bae5f640d73b
huashuolee <huashuolee@gmail.com>
2015-01-13 13:53:33 +0800

    test

--- change.sh
+++ change.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sqlite3 ./settings.db "update system set value='40' where name='screen_brightness';"
+sqlite3 ./settings.db "update system set value='44' where name='screen_brightness';"
 sqlite3 ./settings.db "update system set value='0' where name='accelerometer_rotation'";
